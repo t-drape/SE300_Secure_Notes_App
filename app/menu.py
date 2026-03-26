5fa2aab5538273fd0b0d3838b5b9ffe9a79fe2a6
t-drape <thebatreview1@gmail.com>
2026-03-26 11:21:56 -0700

    Add code for select, Add code for get_password, Add code for act, Add code for possible_actions (list), Add code for confirm_delete, Add test cases for select, Add test cases for get_password, Add test cases for act, Add test cases for confirm_delete

--- app/menu.py
+++ app/menu.py
@@ -3,94 +3,103 @@ print("Welcome to the Secure Notes Application")
 class Menu():
     """This class implements the requirements defined in the SRD and the features defined in the SDD for the Menu Class"""
     def __init__(self):
-        password = self.get_password()
+        # password = self.get_password()
+        print("Hello")
+        # This line allows me to make a function call directly by using the returned user choice as an index (allows passing the file still)
+        self.possible_actions = [self.new_note, self.display, self.append, self.summarize, self.delete]
 
     
-    # def save(file=generate_filename()): 
-    #     if (file):
-    #         print("Hello World")
-    #     else:
-    #         print("No Hello")
+    def save(self, file):
+        pass
 
-    def generate_filename():
-        return "hello_world.txt"
+    def generate_filename(self):
+        pass
+        # return "hello_world.txt"
 
-    def choose_file():
-        print("What file would you like to perform this action on?")
+    def choose_file(self):
+        pass
+        # print("What file would you like to perform this action on?")
 
-    def confirm_delete():
+    def confirm_delete(self):
+        """Ensure user confirms desired deletion"""
         confirmation = False
         answer = input("Are you sure? This action cannot be reversed. [Y or y for yes]\nAnswer: ").lower()
         if (answer == "y"):
             confirmation = True
         return confirmation
     
-    """
-    Test Cases for confirm_delete():
-        Does it work with uppercase y (Y)?
-        Does it work with lowercase y (y)?
-        Do integer values break it?
-        Does it return boolean values?
-    """
+        # Test Cases for confirm_delete():
+            # Does it work with uppercase y (Y)?
+            # Does it work with lowercase y (y)?
+            # Do integer values break it?
+            # Does it return boolean values?
 
-    def new_note(file):
+    def new_note(self, file):
         pass
 
-    def display(file):
+    def display(self, file):
         pass
 
-    def append(file):
+    def append(self, file):
         pass
 
-    def summarize(file):
+    def summarize(self, file):
         pass
 
-    def delete(file):
-        pass
+    def delete(self, file):
+        self.confirm_delete()
+        print("Hello There")
+    
+    # Test cases for delete():
+        # Does the function call confirm_delete before calling DB.delete()?
 
-    def select():
+    def select(self):
+        """Allow user to select their desired action, gracefully handling invalid input"""
         answer = input("""
-What action would you like to perform?
-(Input the number of the selected action)
-    1. Create Note
-    2. Display Note
-    3. Change Note
-    4. Summarize Note
-    5. Delete Note
-                       """)
+    What action would you like to perform?
+    (Input the number of the selected action)
+        1. Create Note
+        2. Display Note
+        3. Change Note
+        4. Summarize Note
+        5. Delete Note
+    Answer: """)
         try:
             answer = int(answer)
+            if answer in range(1,6):
+                return answer
+            else:
+                print("""
+    Invalid Input. 
+    Aborting action and returning to Menu.""")
+                return -1
         except ValueError:
             print("""
-                  Invalid Input. 
-                  Aborting action and returning to Menu.
-                  """)
+    Invalid Input. 
+    Aborting action and returning to Menu.""")
+        return -1
+    
+    # Test Cases for select():
+        # Does it work with valid input (1-5)?
+        # Does it handle numeric values not in range (1-5)?
+        # Does it handle non-numeric inputs?
+        # Does it return an error value if an error occurs?
+        # Does it return a useful value if the code runs properly?
+    
+    def get_password(self):
+        password = input("What is your password? ")
+        return password
+    
+    # Test cases for get_password():
+        # Does it work with no user input?
+        # Does it work with valid user input?
 
-    def get_password():
-        return 0
+    def act(self, selected_action, file="None"):
+        self.possible_actions[selected_action](file)
+    
+    # Test cases for act(selected_action, file):
+        # Does the index match the correct chosen action?
+        # Does the index pass a file correctly?
 
-    def act(selected_action):
-        pass
-
-
-def select():
-    answer = input("""
-What action would you like to perform?
-(Input the number of the selected action)
-    1. Create Note
-    2. Display Note
-    3. Change Note
-    4. Summarize Note
-    5. Delete Note
-Answer: """)
-    try:
-        answer = int(answer)
-        return answer
-    except ValueError:
-        print("""
-Invalid Input. 
-Aborting action and returning to Menu.""")
-    return -1
-
-c = select()
-print(c)
+# m = Menu()
+# m.act(4)
